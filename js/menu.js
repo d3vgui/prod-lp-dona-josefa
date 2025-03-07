04d9f381be234524dca02f2369748e2d0a309843
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-06 21:55:48 -0300

    correção menu mobile

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,17 @@ const btnMobile = document.getElementById('btn-mobile');
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const nav = document.getElementById('nav');
+  const header = document.querySelector('header');
   nav.classList.toggle('active');
   const active = nav.classList.contains('active');
   event.currentTarget.setAttribute('aria-expanded', active);
+  
   if (active) {
     event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    header.style.backgroundColor = 'var(--black-bg)';
   } else {
     event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    header.style.backgroundColor = 'transparent';
   }
 }
 
@@ -20,25 +24,29 @@ const menuLinks = document.querySelectorAll('#menu a');
 
 menuLinks.forEach(link => {
   link.addEventListener('click', function(event) {
-    event.preventDefault(); // Previne o comportamento padrão do link (scroll para a seção)
-
     const nav = document.getElementById('nav');
     const menu = document.getElementById('menu');
 
     
-    menu.style.transition = 'height 0.6s ease-in-out, visibility 0.6s ease-in-out';
-    menu.style.height = '0';  
-    menu.style.visibility = 'hidden'; 
+    if (window.innerWidth <= 735 && nav.classList.contains('active')) {
+      event.preventDefault(); 
 
-    
-    setTimeout(function() {
-      nav.classList.remove('active'); 
-      menu.style.height = ''; 
-      menu.style.visibility = ''; 
-    }, 600); 
-    
-    setTimeout(function() {
-      window.location.href = link.href; 
-    }, 600); 
+      
+      menu.style.transition = 'height 0.4s ease-in-out, visibility 0.4s ease-in-out';
+      menu.style.height = '0';
+      menu.style.visibility = 'hidden';
+
+      
+      setTimeout(() => {
+        nav.classList.remove('active');
+        menu.style.height = '';
+        menu.style.visibility = '';
+      }, 400);
+
+      
+      setTimeout(() => {
+        window.location.href = link.href;
+      }, 400);
+    }
   });
 });
