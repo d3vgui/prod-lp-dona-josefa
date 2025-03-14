92f1f640fa7240659795b7a11da689817371d93a
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-14 20:43:13 -0300

    adiciona carrossel de imagens na sessão 3, ajustes footer e ajustes imagens

--- js/swiper.js
+++ js/swiper.js
@@ -16,3 +16,26 @@ var swiper = new Swiper(".mySwiper",
         prevEl: ".swiper-button-prev",
       },
     })
+
+    var swiper = new Swiper(".mySwiper2", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      loop: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
