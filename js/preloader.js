bec98299f80d5344617961eab72ec79731bdd51a
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-03-17 09:27:30 -0300

    adiciona gsap e alteraçõe de imagens

--- js/preloader.js
+++ js/preloader.js
@@ -1,11 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   let preloader = document.getElementById("preloader");
-//   let body = document.body;
-//   body.classList.add("no-scroll");
+document.addEventListener("DOMContentLoaded", function () {
+  let preloader = document.getElementById("preloader");
+  let body = document.body;
+  body.classList.add("no-scroll");
 
-//   setTimeout(() => {
-//     preloader.style.opacity = "0";
-//     preloader.style.visibility = "hidden";
-//     body.classList.remove("no-scroll");
-//   }, 3000);
-// });
+  setTimeout(() => {
+    preloader.style.opacity = "0";
+    preloader.style.visibility = "hidden";
+    body.classList.remove("no-scroll");
+  }, 3000);
+});
